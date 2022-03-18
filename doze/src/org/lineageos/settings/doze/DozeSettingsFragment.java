1d1e9a1a2510488c3a30e09b69feed12dd43930e
Marc Gading <62163236+Cramx25@users.noreply.github.com>
2022-03-18 19:31:49 +0800

    Update DozeSettingsFragment.java

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -109,8 +109,6 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         boolean dozeEnabled = Utils.isDozeEnabled(getActivity());
 
         mTextView = view.findViewById(R.id.switch_text);
-        mTextView.setText(getString(dozeEnabled ?
-                R.string.switch_bar_on : R.string.switch_bar_off));
 
         mSwitchBar = view.findViewById(R.id.switch_bar);
         Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
@@ -139,8 +137,6 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         Utils.enableDoze(getActivity(), isChecked);
         Utils.checkDozeService(getActivity());
 
-        mTextView.setText(getString(isChecked ? R.string.switch_bar_on : R.string.switch_bar_off));
-        mSwitchBar.setActivated(isChecked);
 
         if (!isChecked) {
             Utils.enableAlwaysOn(getActivity(), false);
